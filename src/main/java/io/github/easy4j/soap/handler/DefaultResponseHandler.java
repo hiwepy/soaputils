41be1873634faa025fa79a302226d4b870b10b63
wandl-6A72h <hiwepy@gmail.com>
2026-07-31 20:38:29 +0800

    refactor: 包名 io.github.hiwepy 改为 io.github.easy4j

---
+++ src/main/java/io/github/easy4j/soap/handler/DefaultResponseHandler.java
@@ -0,0 +1,33 @@
+ package io.github.easy4j.soap.handler;
+
+import java.net.HttpURLConnection;
+
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+
+import io.github.easy4j.soap.SoapFaultUtils;
+
+
+/**
+ * Soap请求响应处理：返回 javax.xml.soap.SOAPMessage 对象
+ * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ */
+public class DefaultResponseHandler implements SoapResponseHandler<SOAPMessage> {
+
+	@Override
+	public void preHandle(HttpURLConnection httpConn) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public SOAPMessage handleResponse(SOAPMessage response) throws SOAPException {
+		// 第一步检查结果
+		SoapFaultUtils.checkFault(response);
+		
+		return response;
+	}
+	
+}
+
+ 
---
+++ src/main/java/io/github/easy4j/soap/handler/PlainTextResponseHandler.java
@@ -0,0 +1,26 @@
+package io.github.easy4j.soap.handler;
+
+import java.net.HttpURLConnection;
+
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+
+/**
+ * Soap请求响应处理：返回java.lang.String对象
+ * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ */
+public class PlainTextResponseHandler implements SoapResponseHandler<String> {
+
+	@Override
+	public void preHandle(HttpURLConnection httpConn) {
+		
+	} 
+
+	@Override
+	public String handleResponse(SOAPMessage response) throws SOAPException {
+		
+		
+		return null;
+	}
+
+}
