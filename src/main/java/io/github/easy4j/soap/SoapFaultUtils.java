701105b8cc227bde00c3d746c37715858729cee9
wandl-6A72h <hiwepy@gmail.com>
2026-07-31 20:38:27 +0800

    refactor: 包名 io.github.hiwepy 改为 io.github.easy4j

---
+++ src/main/java/io/github/easy4j/soap/SoapFaultUtils.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.github.easy4j.soap;
+
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPFault;
+import javax.xml.soap.SOAPMessage;
+
+import io.github.easy4j.soap.exception.InvokeException;
+
+public class SoapFaultUtils {
+
+	public static void checkFault(SOAPMessage response) throws SOAPException {
+		SOAPEnvelope envelope = response.getSOAPPart().getEnvelope();
+		SOAPBody body = envelope.getBody();
+		SOAPFault fault = body.getFault();
+		if ((fault != null) && (fault.getFaultCode() != null)) {
+			throw new InvokeException(fault.getFaultCode(), fault.getFaultString());
+		}
+	}
+	
+}
