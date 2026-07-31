61b1cdafc22d7a8a298171993c2ae240d27cd634
wandl-6A72h <hiwepy@gmail.com>
2026-07-31 20:38:28 +0800

    refactor: 包名 io.github.hiwepy 改为 io.github.easy4j

---
+++ src/main/java/io/github/easy4j/soap/handler/XMLResponseHandler.java
@@ -0,0 +1,35 @@
+ package io.github.easy4j.soap.handler;
+
+import java.net.HttpURLConnection;
+
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+
+import org.w3c.dom.Document;
+
+/**
+ * Soap请求响应处理：返回org.w3c.dom.Document对象
+ * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ */
+public class XMLResponseHandler implements SoapResponseHandler<Document> {
+
+	@Override
+	public void preHandle(HttpURLConnection httpConn) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Document handleResponse(SOAPMessage response) throws SOAPException {
+		// 响应消息处理,将响应的消息转换为doc对象  
+		Document doc = response.getSOAPPart().getEnvelope().getBody()  
+		        .extractContentAsDocument();  
+		
+		return doc;
+	}
+
+	
+	
+}
+
+ 
